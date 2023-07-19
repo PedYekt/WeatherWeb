16d1f1ec4d9e6634f897a22ffcb034cff46fc283
PedYekt <pedram.yekta12@gmail.com>
2023-07-19 17:38:13 -0400

    Added functionality to search button

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-// const title = document.querySelector('#main-title');
-// title.style.color = 'red';
-// console.log(title);
+const revealBtn = document.querySelector('.content button');
+const hiddenContent = document.querySelector('.content img')
 
+function revealContent() {
+    if(hiddenContent.classList.contains('revealBtn')
+    ){
+        hiddenContent.classList.remove('revealBtn');
+        console.log("true");
+    }
+    else{
+        hiddenContent.classList.add('revealBtn');
+        console.log("false");
+    }
+}
+
+revealBtn.addEventListener('click',revealContent);
